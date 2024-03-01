142677d5a885f1a2924b74209ad06a67788dc76e
Bishnu Nanda <bishnunandap@gmail.com>
2024-03-01 21:07:54 +0530

    changes

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.12"
+      version = "3.0.0"
     }
   }
 }
@@ -15,6 +15,7 @@ provider "azurerm" {
   name     = "${var.prefix}-resources"
   location = var.location
 }
+
 
 
 
